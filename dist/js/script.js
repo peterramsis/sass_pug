7adbd7ddf8b46365d63ba81c8e974e8c1c2138c6
peterramsis <peterzizo100@gmail.com>
2018-11-15 15:25:54 +0200

    project sass and pug

---
+++ dist/js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+    "use strict";
+    
+    $(window).scroll(function(){
+         
+        if($(window).scrollTop() > 20){
+
+            $("header").addClass("sticky");
+
+        }else{
+            $("header").removeClass("sticky");
+        }
+
+    });
+
+})
